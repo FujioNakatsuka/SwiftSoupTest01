e9d1a7b65df29f45a36f0dc9561a81d1d42570a6
Nakatsuka Fujio <ichifuji.2taka@ezweb.ne.jp>
2020-10-03 19:33:49 +0900

    Youtubeで公開されている’Swift Soup Tutorialで作成 pod installの際に、The `ProjectName [Debug Stage]` target overrides the `ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES` build setting defined in `Pods/Target Support Files/Pods-ProjectName/Pods-ProjectName.debug stage.xcconfig'. This can lead to problems with the CocoaPods installation     - Use the `$(inherited)` flag, or     - Remove the build settings from the target. という警告が出る。TestUIに上記の変更を加えて、無事に通過！

---
+++ Pods/Target Support Files/Pods-SwiftSoupTest01-SwiftSoupTest01UITests/Pods-SwiftSoupTest01-SwiftSoupTest01UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftSoupTest01_SwiftSoupTest01UITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftSoupTest01_SwiftSoupTest01UITests
+@end
--- SwiftSoupTest01/ViewController.swift
+++ SwiftSoupTest01/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        do {
+          let html = "<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p><a class='mylink'  id='applelink' href='http://applel.com'>Link to apple's website</a><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li></ul></body></html>"
+            
+            let doc: Document = try SwiftSoup.parse(html)
+            
+            let p:Element = try doc.select("p").first()!
+            print(try p.text())
+            let a: Element = try doc.select("a").first()!
+            let link: String = try a.attr("href")
+            let text:String = try a.text()
+            let className: String = try a.className()
+            let idName: String = a.id()
+            let li:[Element] = try doc.select("li").array()
+            print(li[2])
+            
+            
+            print(link)
+            print(text)
+            print(className)
+            print(idName)
+           
+            
+        } catch Exception.Error(type: let type, Message: let message) {
+            print(type)
+            print(message)
+        }catch{
+            print("")
+        }
+        
     }
 
+    
+    
+    
 
 }
 
