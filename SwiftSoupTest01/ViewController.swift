d82cb8f37bd371d3b6b2c4666252a18bcd4471ab
Nakatsuka Fujio <ichifuji.2taka@ezweb.ne.jp>
2020-10-03 17:54:12 +0900

    Initial Commit

---
+++ SwiftSoupTest01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSoupTest01
+//
+//  Created by 中塚富士雄 on 2020/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
